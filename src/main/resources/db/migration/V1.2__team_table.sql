9e04e946440b52f44a81e69c640974854b827611
Soleyman Shahir <Soleymanshahir@yahoo.com>
2019-05-13 15:24:35 +0100

    updated home page with new UI

--- src/main/resources/db/migration/V1.2__team_table.sql
+++ src/main/resources/db/migration/V1.2__team_table.sql
@@ -2,6 +2,5 @@ DROP TABLE IF EXISTS team;
 
 CREATE TABLE team (
   id bigserial PRIMARY KEY,
-  teamname varchar(250) NOT NULL,
-  teamcode varchar(250) NOT NULL
+  teamname varchar(250) NOT NULL
 );
