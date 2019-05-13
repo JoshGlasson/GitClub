a651419d02189b90871659f465dd369b59cd60f9
Josh Glasson <Josh.Glasson@tesco.com>
2019-05-13 12:02:34 +0100

    added user class and sign up page

--- src/main/java/com/gitclub/GitClub/controller/HomeController.java
+++ src/main/java/com/gitclub/GitClub/controller/HomeController.java
@@ -1,11 +1,15 @@
 package com.gitclub.GitClub.controller;
 
+import com.gitclub.GitClub.model.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.view.RedirectView;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 
 @Controller
 public class HomeController {
@@ -18,5 +22,17 @@ public class HomeController {
     @RequestMapping(value = "/")
     public String index() {
         return "index";
+    }
+
+    @GetMapping(value = "user/new/manager")
+    public String user(Model model) {
+        model.addAttribute("user", new ManagerForm("", "", "", "manager", ""));
+        return "register_manager";
+    }
+
+    @PostMapping(value = "manager/manager")
+    public RedirectView user(@ModelAttribute Manager manager) {
+//        userRepository.save(manager);
+        return new RedirectView("/");
     }
 }
---
+++ src/main/java/com/gitclub/GitClub/model/Manager.java
@@ -0,0 +1,49 @@
+package com.gitclub.GitClub.model;
+
+import lombok.Data;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "USERS")
+public class Manager {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+    private String role;
+    private String position;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long teamid;
+
+    private Manager() {
+
+    }
+
+    public Manager(String name, String email, String password, String role, String position) {
+        this.name = name;
+        this.email = email;
+        this.password = this.setPassword(password);
+        this.role = role;
+        this.position = position;
+    }
+
+    public String setPassword(String password) {
+       return this.password = BCrypt.hashpw(password, BCrypt.gensalt());
+    }
+
+    public String getEmail(){
+        return this.email;
+    }
+
+    public String getPassword(){
+        return this.password;
+    }
+
+}
+
