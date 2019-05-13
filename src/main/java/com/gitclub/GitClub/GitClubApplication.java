93c6fef201543ad2be63144a381ed9f0bcb3ae2d
Josh Glasson <Josh.Glasson@tesco.com>
2019-05-13 10:27:40 +0100

    added project files

---
+++ src/main/java/com/gitclub/GitClub/GitClubApplication.java
@@ -0,0 +1,13 @@
+package com.gitclub.GitClub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitClubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitClubApplication.class, args);
+	}
+
+}
