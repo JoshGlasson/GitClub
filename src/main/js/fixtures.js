084f757f6463ad62809c837c760b2db828214a98
Phil <phil.board90@googlemail.com>
2019-05-16 12:09:27 +0100

    Added delete button for fixtures

--- src/main/js/fixtures.js
+++ src/main/js/fixtures.js
@@ -1,18 +1,35 @@
 import React, {Component} from 'react';
 
 class Fixtures extends Component {
+constructor(props){
+    super(props);
+    this.state = {fixtureid: this.props.item._links.self.href.split("/")[this.props.item._links.self.href.split("/").length-1] };
+    this.deleteFixture = this.deleteFixture.bind(this);
+    }
     render() {
         return (
             <div>
 
-                 <p>| {this.props.item.fixture} | {this.props.item.date} | {this.props.item.location} |</p>
-
+                 <p>| {this.props.item.fixture} | {this.props.item.date} | {this.props.item.location} | <button type="button" class="btn btn-primary" onClick={this.deleteFixture} >Delete</button></p>
 
             </div>
         );
-
-
     }
+
+        deleteFixture(){
+         console.log("Pre Fetch")
+                       fetch('/api/fixtureses/'+ this.state.fixtureid, {
+                                       method: 'DELETE',
+                                       headers: {
+                                         'Accept': 'application/json',
+                                         'Content-Type': 'application/json',
+                                       },
+                                     })
+                                     location.href = location;
+
+
+
+                   };
 }
 
 export default Fixtures;
