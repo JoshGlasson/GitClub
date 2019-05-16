17b80389d642db81b048cbb828cc1522ae014995
Josh Glasson <Josh.Glasson@tesco.com>
2019-05-16 14:34:59 +0100

    calendar shows, data from api doesnt work

--- src/main/js/calendar.js
+++ src/main/js/calendar.js
@@ -4,31 +4,7 @@ import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import listPlugin from '@fullcalendar/list';
 
-var data = fetch('api/calendars/search/findByTeamid?teamid='+ document.getElementById("teamid").value, {
-               method: 'GET',
-               headers: {
-                     'Content-Type': 'application/json',
-                     },
-               credentials: 'same-origin'
-             })
-             .then((response) => {
-               if(response.ok) {
-                 return response.json();
-               } else {
-                 throw new Error('Server response wasn\'t OK');
-               }
-             })
-             .then(function(json) {
-                if(json) {
-                  return json._embedded.calendars;
-                } else {
-                  throw new Error('Server response wasn\'t OK');
-                }
-             });
-
 document.addEventListener('DOMContentLoaded', function() {
-
-    console.log(document.getElementById('data'))
 
   var calendarEl = document.getElementById('calendar');
 
@@ -44,9 +20,35 @@ document.addEventListener('DOMContentLoaded', function() {
     navLinks: true, // can click day/week names to navigate views
     editable: true,
     eventLimit: true, // allow "more" link when too many events
-    events: [{id:1, title: "Josh FC Training", start: "2019-05-23T14:30:00", color: 'red', teamid: 1, _links: {self: {href: "http://localhost:8080/api/calendars/1"}}}]
+    events: (fetch('api/calendars/search/findByTeamid?teamid='+ document.getElementById("teamid").value, {
+                                         method: 'GET',
+                                         headers: {
+                                               'Content-Type': 'application/json',
+                                               },
+                                         credentials: 'same-origin'
+                                       })
+                                       .then((response) => {return response.json()})
+                                       .then(function(json) {
+                                       console.log(JSON.parse(JSON.stringify(json._embedded.calendars)));
+                                       return JSON.parse(JSON.stringify(json._embedded.calendars))}))
     });
 
   calendar.render();
 });
 
+//[
+//{title: "Josh FC v Other Team", start: "2019-05-22T19:00:00", color: "blue", teamid: 1, _links: {self: {href: "http://localhost:8080/api/calendars/2"}, calendar: {href: "http://localhost:8080/api/calendars/2"}}},
+//{title: "Josh FC Training", start: "2019-05-19T12:00:00", color: "red", teamid: 1, _links: {self: {href: "http://localhost:8080/api/calendars/1"}, calendar: {href: "http://localhost:8080/api/calendars/1"}}},
+//]
+//
+//(fetch('api/calendars/search/findByTeamid?teamid='+ document.getElementById("teamid").value, {
+//                             method: 'GET',
+//                             headers: {
+//                                   'Content-Type': 'application/json',
+//                                   },
+//                             credentials: 'same-origin'
+//                           })
+//                           .then((response) => {return response.json()})
+//                           .then(function(json) {
+//                           console.log(JSON.parse(JSON.stringify(json._embedded.calendars)));
+//                           return JSON.parse(JSON.stringify(json._embedded.calendars))}))
