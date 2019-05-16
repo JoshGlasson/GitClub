ba9df2a56067257b2006f50169f14ceb211acb6d
seanbanford <seanbanford@gmail.com>
2019-05-16 15:36:13 +0100

    second type table added and working

--- src/main/js/availabilityTable.js
+++ src/main/js/availabilityTable.js
@@ -1,6 +1,6 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
-
+import { MDBTable, MDBTableBody, MDBTableHead } from 'mdbreact';
 
 
 class Table extends React.Component {
@@ -53,6 +53,44 @@ class Table extends React.Component {
    }
 }
 console.log("Put a message here.")
-ReactDOM.render(<Table />, document.getElementById('availability'));
+
+
+const BasicTable = props => {
+  return (
+    <MDBTable>
+      <MDBTableHead>
+        <tr>
+          <th>#</th>
+          <th>First</th>
+          <th>Last</th>
+          <th>Handle</th>
+        </tr>
+      </MDBTableHead>
+      <MDBTableBody>
+        <tr>
+          <td>1</td>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+        </tr>
+        <tr>
+          <td>2</td>
+          <td>Jacob</td>
+          <td>Thornton</td>
+          <td>@fat</td>
+        </tr>
+        <tr>
+          <td>3</td>
+          <td>Larry</td>
+          <td>the Bird</td>
+          <td>@twitter</td>
+        </tr>
+      </MDBTableBody>
+    </MDBTable>
+  );
+}
+
+export default BasicTable;
+ReactDOM.render(<BasicTable />, document.getElementById('availability'));
 
 
